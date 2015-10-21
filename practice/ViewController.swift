2525be4bc26cc122e3f0f9ac486ac59be634c382
e145751 <e145751@yamamotoshinoge-no-MacBook-Pro.local>
2015-10-22 01:11:54 +0900

    練習
    練習

---
+++ practice/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  practice
+//
+//  Created by 山本　凌 on 10/22/15.
+//  Copyright (c) 2015 山本　凌. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
